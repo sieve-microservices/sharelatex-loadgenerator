0fb4b3790865f4a5199fef00de757c6b712ccc17
Jörg Thalheim <joerg@higgsboson.tk>
2016-06-10 12:38:53 +0000

    cleanup delete db script

--- delete-db.py
+++ delete-db.py
@@ -2,8 +2,3 @@ from influxdb import InfluxDBClient
 c = InfluxDBClient("influxdb.local", 8086, "influxdb", "rewtrewt", "metrics")
 c.query("drop database metrics")
 c.query("create database metrics")
-#for batch in c.query("show measurements"):
-#    for m in batch:
-#        q = 'drop measurement "%s"' % (m["name"].replace('\\', '\\\\'))
-#        print(q)
-#        c.query(q)
