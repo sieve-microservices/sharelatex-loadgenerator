be8c594f6b11e6857eb1f5afb6b7274df705427b
Ubuntu <ubuntu@ip-192-168-8-208.local>
2016-10-20 16:13:51 +0000

    prepare-network.sh: use dockers ip range instead

--- prepare-network.sh
+++ prepare-network.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
+# random source ips to get around rate limiter
 set -e
 
 IF=${IF:-eth0:avahi}
 
 for i in $(seq 1 50); do
-  ip addr add "169.254.75.$i/16" dev $IF scope link || true
+  ip addr add "172.17.99.$i/16" dev $IF || true
 done
 
 iptables -t nat -F RANDSRC || iptables -t nat -N RANDSRC
@@ -14,5 +15,5 @@ iptables -t nat -I POSTROUTING -p tcp --dport 80 -j RANDSRC
 
 ip=$(getent hosts loadbalancer.local | awk '{print $1}')
 for i in $(seq 50 -1 2); do
-  iptables -t nat -A RANDSRC -d "$ip" -m statistic --mode nth --every "$i" --packet 0 -j SNAT --to-source 169.254.75.$i
+  iptables -t nat -A RANDSRC -d "$ip" -m statistic --mode nth --every "$i" --packet 0 -j SNAT --to-source 172.17.99.$i
 done
